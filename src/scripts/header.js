010f4fe2a7d22ad17af1dcca6d31d1ece0f25c42
MauroLozano <mauroloz2006@gmail.com>
2025-04-26 13:32:30 -0300

    Stylized the background image for the header in layout.css. Added header.js to create movment for the background image

---
+++ src/scripts/header.js
@@ -0,0 +1,31 @@
+const header = document.getElementById('header');
+const background_image = document.getElementById('background-image');
+const vw = document.documentElement.clientWidth;
+const vh = document.documentElement.clientHeight;
+let mousePosPercentageX = null;
+let mousePosPercentageY = null;
+let currentTOP = null;
+let currentLeft = null;
+let isMouseOver=false;
+header.addEventListener('mouseover', ()=>{ //Detects if the mouse is over the header
+    let isMouseOver = true;
+    console.log(isMouseOver);
+    header.addEventListener('mousemove', function(event) { //Get the coordinates from the mouse position.
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        //console.log('X: ',mouseX,' Y: ',mouseY);
+        mousePosPercentageX = (mouseX*100)/vw;  //Transform the coordinates to percentages.w
+        mousePosPercentageY = (mouseY*100)/vh;
+        // console.log('X: ',mousePosPercentageX,' Y: ',mousePosPercentageY);
+        background_image.style.top=`${currentTOP}%`;
+        background_image.style.left=`${currentLeft}%`;
+        // console.log(currentLeft);
+        // console.log(currentRight);
+        //Use the distance between the cursor and the margins of the header to calculate the distance in % of the background_image from the margins.
+        currentTOP = -50 + (((50-mousePosPercentageY)/50))*-1;
+        currentLeft= -1+ (((mousePosPercentageX-50)/50))*1;
+    });
+});
+header.addEventListener('mouseout', ()=>{ //Detects if the mouse is not over the header
+    console.log(isMouseOver)
+});
