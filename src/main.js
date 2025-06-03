d4e0661174e2b00ed696d132229d1dffd79d3c74
MauroLozano <mauroloz2006@gmail.com>
2025-06-03 09:40:59 -0300

    Reestructured the whole page with css grid. Added content to the presentations section. Modified existing responsive design rules and added new ones. Added images for later use.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import headerMovement from "./headerMovement.js";
 import scrollNavigation from "./scrollNavigation.js";
 import animations from "./animations.js";
 import slider from "./slider.js";
-slider()
+slider();
 headerMovement();
 scrollNavigation();
 animations();
