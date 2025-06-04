73ea2fb74010c59f21d4768f4dfec8bd1c8ef934
MauroLozano <mauroloz2006@gmail.com>
2025-06-03 22:02:59 -0300

    Added a footer

--- src/animations.js
+++ src/animations.js
@@ -5,6 +5,7 @@ export default function animations(){
     const partners__title = document.querySelector('.partners__title');
     const presentation = document.querySelector('.presentation');
     const presentation_title = document.querySelector('.presentation__title');
+    const footer = document.footer;
     const mutations_observer = new MutationObserver(function(mutations) { //It waits for the changes for the DOM to be made before running the code.
         if(feat_prod.classList.contains('active')){             //Otherwise the if won't find the class active.
             feat_prod_title.classList.add('slideInLeft');
@@ -14,6 +15,9 @@ export default function animations(){
         }
         if(presentation.classList.contains('active')){
             presentation_title.classList.add('slideInDown');
+        }
+        if(footer.classList.contains('active')){
+            presentation.classList.add
         }
     });
     mutations_observer.observe(feat_prod,partners,presentation, { attributes: true, attributeFilter: ['class'] });
